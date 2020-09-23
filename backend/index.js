d3572ee265661e6280589b7873dc705fa9ccc973
to-na <tonacoin@gmail.com>
2020-09-23 12:56:31 +0900

    fix: fix backend to get box by 6 digit userId

--- backend/index.js
+++ backend/index.js
@@ -3,40 +3,47 @@ const express = require('express');
 const app = express();
 const PRIORITY = require('./constants');
 
-const messages = [
-  [{
-    id: 1,
-    sender: 'foo',
-    sendTimeISO: '2020-09-01T12:00:00+09:00',
-    title: 'title desu',
-    priority: PRIORITY.URGENT,
+const messageBoxes = [
+  {
+    userId: 123456,
+    mails: [{
+      id: 1,
+      sender: 'foo',
+      sendTimeISO: '2020-09-01T12:00:00+09:00',
+      title: 'title desu',
+      priority: PRIORITY.URGENT,
+    },
+    {
+      id: 2,
+      sender: 'bar',
+      sendTimeISO: '2020-09-02T12:00:00+09:00',
+      title: 'title2 desu',
+      priority: PRIORITY.HIGH,
+    }],
   },
   {
-    id: 2,
-    sender: 'bar',
-    sendTimeISO: '2020-09-02T12:00:00+09:00',
-    title: 'title2 desu',
-    priority: PRIORITY.HIGH,
-  }],
-  [{
-    id: 3,
-    sender: 'baz',
-    sendTimeISO: '2020-09-03T12:00:00+09:00',
-    title: 'title3 desu',
-    priority: PRIORITY.MIDDLE,
+    userId: 234567,
+    mails: [{
+      id: 3,
+      sender: 'baz',
+      sendTimeISO: '2020-09-03T12:00:00+09:00',
+      title: 'title3 desu',
+      priority: PRIORITY.MIDDLE,
+    },
+    {
+      id: 4,
+      sender: 'hoge',
+      sendTimeISO: '2020-09-04T12:00:00+09:00',
+      title: 'title4 desu',
+      priority: PRIORITY.LOW,
+    }],
   },
-  {
-    id: 4,
-    sender: 'hoge',
-    sendTimeISO: '2020-09-04T12:00:00+09:00',
-    title: 'title4 desu',
-    priority: PRIORITY.LOW,
-  }],
 ]
 
 app.get('/api/v1/inbox/:id', function (req, res) {
   try {
-    const payload = JSON.stringify(messages[Number(req.params.id)]);
+    const messages = messageBoxes.find(item => item.userId === Number(req.params.id));
+    const payload = JSON.stringify(messages.mails);
     console.log(payload);
     const token = jwt.sign(payload, 'secret');
     console.log(token);
